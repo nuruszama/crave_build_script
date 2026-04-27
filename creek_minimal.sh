6d8e31cab83d9ce0fe1216087be74fefe3372b93
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-04-27 22:38:51 +0400

    Update creek_minimal.sh

--- creek_minimal.sh
+++ creek_minimal.sh
@@ -1,61 +1,66 @@
 #!/bin/bash
 
 Load env
-
 source .env
 
 echo "[$(date +%T)] Starting Minimal Boot Build..."
 
 crave run --projectID 93 --no-patch -- '
+  echo "================================="
+  echo "     Minimal Creek Boot Build"
+  echo "================================="
 
-echo "================================="
-echo "     Minimal Creek Boot Build"
-echo "================================="
+  # Clean only what matters
+  rm -rf out/target/product/creek
+  repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 
-Clean only what matters
+  # Local manifests
+  rm -rf .repo/local_manifests
+  git clone https://github.com/nuruszama/local_manifest.git -b main .repo/local_manifests
 
-rm -rf out/target/product/creek
+  # Sync
+  /opt/crave/resync.sh
 
-Init repo
+  # Env setup
+  source build/envsetup.sh
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+  # Lunch
+  lunch lineage_creek-trunk_staging-userdebug
 
-Local manifests
+  # Clean intermediates only
+  make installclean
 
-git clone https://github.com/nuruszama/local_manifest.git -b main .repo/local_manifests
+  echo "================================="
+  echo "      Building bootimage ONLY"
+  echo "================================="
 
-Sync
-
-/opt/crave/resync.sh
-
-Env setup
-
-source build/envsetup.sh
-
-Lunch
-
-lunch lineage_creek-trunk_staging-userdebug
-
-Clean intermediates only
-
-make installclean
-
-echo "================================="
-echo "      Building bootimage ONLY"
-echo "================================="
-
-🔥 ONLY build boot image
-
-mka bootimage
-
-'
+  # 🔥 ONLY build boot image
+  mka bootimage
+  mka systemimage'
 
 EXIT_STATUS=$?
-
 echo "Build finished with status: $EXIT_STATUS"
 
 if [ $EXIT_STATUS -eq 0 ]; then
-echo "✅ boot.img build SUCCESS"
+    # SUCCESS
+    crave run --projectID 93 -- 'curl -s -X POST "https://api.telegram.org/bot'"$TG_TOKEN"'/sendDocument" \
+      -F chat_id="'"$TG_CHAT"'" \
+      -F document=@"out/target/product/creek/boot.img '
+    echo "Build Completed.. boot.img ready to download......."
+    curl -s -o /dev/null -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT" -d parse_mode="HTML" \
+        -d text="✅ <b>Build Success!</b>%0A📦"
+
+elif [ $EXIT_STATUS -eq 130 ]; then
+    # CANCELLED BY USER
+    curl -s -o /dev/null -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT" -d parse_mode="HTML" \
+        -d text="⚠️ <b>Build Cancelled:</b> User terminated the process manually."
+
 else
-echo "❌ build FAILED"
+    # FAILED DUE TO ERROR
+    echo ">>Build Failed"
+    curl -s -o /dev/null -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT" -d parse_mode="HTML" \
+        -d text="<b>Build Failed (Error $EXIT_STATUS)</b>"
 fi
